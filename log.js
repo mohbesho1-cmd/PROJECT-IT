a3da1878f32024ce5afaae222f38fc55ceaec8a7
yousefahmedabdallah2-tech <yousefahmedabdallah2@gmail.com>
2026-05-02 19:21:32 +0300

    Edited by yousef- Hatem
    Updated toggleTheme function to manage body class for dark mode.

--- log.js
+++ log.js
@@ -4,9 +4,11 @@ function toggleTheme() {
     if (theme.getAttribute("href") === "dark.css") {
         theme.setAttribute("href", "white.css");
         localStorage.setItem("theme", "light");
+        document.body.classList.remove("dark");
     } else {
         theme.setAttribute("href", "dark.css");
         localStorage.setItem("theme", "dark");
+        document.body.classList.add("dark");
     }
 }
 
@@ -16,7 +18,9 @@ window.onload = function () {
 
     if (saved === "light") {
         document.getElementById("theme-style").href = "white.css";
+        document.body.classList.remove("dark");
     } else {
         document.getElementById("theme-style").href = "dark.css";
+        document.body.classList.add("dark");
     }
 };
